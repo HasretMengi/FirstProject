477b377058d4568f0a732e0c963038d52e3173d0
hasret <rabiamengi1@hotmail.com>
2023-09-12 10:45:22 -0400

    Fixed id increment

--- Alchemy/Models/Robot.cs
+++ Alchemy/Models/Robot.cs
@@ -2,7 +2,7 @@ namespace Alchemy.Models
 {
     public class Robot
     {
-        private static int _currentId = 1;
+
         public int Id { get; set; }
         public string UrlImage { get; set; }
         public string Nom { get; set; }
@@ -13,11 +13,8 @@ namespace Alchemy.Models
         // Propriété statique pour suivre le prochain ID
         public static int NextId { get; private set; } = 4;
 
-       // Constructeur qui incrémente automatiquement l'ID
-        public Robot()
-        {
-            Id = _currentId++;
-        }
-
+       
     }
+
+    public record Robot2(int Id,string Nom, string UrlImage, string Taille,string Poids,string Pays);
 }
